84a486eaa04183201a908bdde95768233bcce913
Arya Das <aryadas98@gmail.com>
2017-12-02 15:12:17 +0530

    UI Enhancements (#8)
    * Prevents fab from blocking content.
    
    * Code cleanup
    
    * Code cleanup #2

--- app/src/main/java/com/riteshmohapatra/wikipediareader/MainActivity.java
+++ app/src/main/java/com/riteshmohapatra/wikipediareader/MainActivity.java
@@ -4,18 +4,13 @@ import android.os.Bundle;
 import android.speech.tts.TextToSpeech;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v4.content.ContextCompat;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.View.OnFocusChangeListener;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -39,16 +34,12 @@ public class MainActivity extends AppCompatActivity {
     private RequestQueue queue;
     private TextToSpeech tts;
 
-    // search
-    private Toolbar toolbar;
-    private MenuItem searchBtn;
-    private boolean isSearchOpen;
-
     // viewer
-    TextView textView;
-    ProgressBar progress;
-    View textViewer;
-    FloatingActionButton fab;
+    private TextView textView;
+    private ProgressBar progress;
+    private View textViewer;
+
+    private FloatingActionButton fab;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -71,11 +62,9 @@ public class MainActivity extends AppCompatActivity {
         progress = (ProgressBar) findViewById(R.id.progressBar);
         textViewer = findViewById(R.id.textViewer);
         fab = (FloatingActionButton) findViewById(R.id.volume);
-        toolbar = (Toolbar)findViewById(R.id.toolbar);
 
+        Toolbar toolbar = (Toolbar)findViewById(R.id.toolbar);
         setSupportActionBar(toolbar); // set toolbar as the ActionBar
-
-        isSearchOpen = false; // what is shown - search box or activity title
 
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -113,28 +102,40 @@ public class MainActivity extends AppCompatActivity {
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu,menu);
+
+        final MenuItem searchMenuItem = menu.findItem(R.id.action_search);
+        final SearchView searchView = (SearchView)searchMenuItem.getActionView();
+        searchView.setQueryHint("Search Wikipedia");
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override       // what happens when the user submits a query
+            public boolean onQueryTextSubmit(String query) {
+                searchMenuItem.collapseActionView(); // collapse the search box
+                if (tts.isSpeaking()) {     // if tts is speaking, stop it
+                    tts.stop();
+                    fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_play_arrow));
+                }
+                search(query);
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String query) {
+                // do nothing
+                return false;
+            }
+        });
+
+        searchView.setOnQueryTextFocusChangeListener(new OnFocusChangeListener() {
+            @Override       // hide the search box if the user focuses on something else
+            public void onFocusChange(View view, boolean queryTextFocused) {
+                if(!queryTextFocused) {
+                    searchView.setIconified(true);
+                    searchMenuItem.collapseActionView();
+                }
+            }
+        });
+
         return true;
-    }
-
-    @Override
-    public boolean onPrepareOptionsMenu(Menu menu) {
-        searchBtn = menu.findItem(R.id.search_btn);
-        return super.onPrepareOptionsMenu(menu);
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        if (item.getItemId() == R.id.search_btn)
-            handleSearchButton();
-        return super.onOptionsItemSelected(item);
-    }
-
-    @Override
-    public void onBackPressed() {
-        if(isSearchOpen)
-            hideSearchBox();
-        else
-            super.onBackPressed();
     }
 
     private void search(String query) {        // fetches the article and loads it into the viewer.
@@ -177,87 +178,9 @@ public class MainActivity extends AppCompatActivity {
         MainActivity.this.queue.add(jsObjRequest);
     }
 
-    // Source: http://blog.rhesoft.com/2015/03/30/tutorial-android-actionbar-with-material-design-and-search-field/
-    private void handleSearchButton() {
-        ActionBar action = getSupportActionBar(); //get the actionbar
-
-        if(isSearchOpen){ // if search box is shown, the cross button just clears the text
-            EditText searchBox = (EditText)action.getCustomView().findViewById(R.id.search_box);
-            searchBox.getText().clear();
-
-            // set focus to the search box and bring up the keyboard in case it wasn't up.
-            searchBox.requestFocus();
-            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            imm.showSoftInput(searchBox, InputMethodManager.SHOW_IMPLICIT);
-
-        } else { // if search box is not shown, show the search box
-            action.setDisplayShowCustomEnabled(true);   // enable a custom view in the action bar.
-            action.setCustomView(R.layout.search_box);  // add the custom view (search box)
-            action.setDisplayShowTitleEnabled(false);   // hide the title
-
-            EditText searchBox = (EditText)action.getCustomView().findViewById(R.id.search_box);
-            searchBox.requestFocus();
-
-            // bring up the keyboard
-            InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-            imm.showSoftInput(searchBox, InputMethodManager.SHOW_IMPLICIT);
-
-            // a listener to perform the actual search
-            searchBox.setOnEditorActionListener(new TextView.OnEditorActionListener() {
-                @Override
-                public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
-                    if (actionId == EditorInfo.IME_ACTION_SEARCH) {
-                        String query = v.getText().toString().trim();   // get the query
-                        hideSearchBox();    // hide the search box
-                        if (tts.isSpeaking()) {     // stop the tts
-                            tts.stop();
-                            fab.setImageDrawable(ContextCompat.getDrawable(MainActivity.this, R.drawable.ic_play_arrow));
-                        }
-                        search(query);      // search the query
-                        return true;
-                    }
-                    return false;
-                }
-            });
-
-            // a listener to hide the search box if the user clicks somewhere else.
-            searchBox.setOnFocusChangeListener(new OnFocusChangeListener() {
-                @Override
-                public void onFocusChange(View v, boolean hasFocus) {
-                    if (!hasFocus) {
-                        hideSearchBox();
-                    }
-                }
-            });
-
-            // change search icon to cross
-            searchBtn.setIcon(getResources().getDrawable(R.drawable.ic_cancel_search));
-
-            isSearchOpen = true;
-        }
-    }
-
-    private void hideSearchBox() {                 // hides search box and shows title
-        ActionBar action = getSupportActionBar();  // get the actionbar
-
-        action.setDisplayShowCustomEnabled(false); // disable a custom view inside the actionbar
-        action.setDisplayShowTitleEnabled(true);   // show the title in the action bar
-
-        // hide the keyboard
-        InputMethodManager imm = (InputMethodManager) getSystemService(INPUT_METHOD_SERVICE);
-        View view = this.getCurrentFocus();
-        if (view != null)
-            imm.hideSoftInputFromWindow(view.getWindowToken(),0);
-
-        // change icon in the action bar to search
-        searchBtn.setIcon(getResources().getDrawable(R.drawable.ic_open_search));
-
-        isSearchOpen = false;   // search box is not shown anymore
-    }
-
     private static String encodeURIComponent(String s)
     {
-        String result = null;
+        String result;
 
         try
         {
